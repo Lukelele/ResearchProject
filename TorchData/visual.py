9c539cdc8a9ed84bf858f3975876cf7f614ffedc
CATFOOL <779191518s@gmail.com>
2025-02-25 19:41:19 +0000

    [feat] visual remove unused axes

--- TorchData/visual.py
+++ TorchData/visual.py
@@ -17,11 +17,14 @@ accept_fields = Literal[
     'sn', 'sn_time', 'sn_time_norm',
 ]
 
-def plot2d(data_list, titles: List[str]):
+def plot2d(data_list, titles: List[str], cmap='viridis'):
     num = len(data_list)
-    nrows = math.ceil(num / 3)
-    figsize = (14, 4 * nrows)
-    fig, ax = plt.subplots(nrows, 3, figsize=figsize, squeeze=False)
+    ncols = 3
+    if num < ncols:
+        ncols = num
+    nrows = math.ceil(num / ncols)
+    figsize = (4.67 * ncols, 4 * nrows)
+    fig, ax = plt.subplots(nrows, ncols, figsize=figsize, squeeze=False)
     im = None
     zmin, zmax = None, None
 
@@ -33,24 +36,31 @@ def plot2d(data_list, titles: List[str]):
         if type(data) == torch.Tensor:
             data = data.detach().cpu().numpy()
         data = data.squeeze()
-        im = ax[i // 3, i % 3].imshow(data, origin='lower', cmap='blues', vmin=zmin, vmax=zmax)
+        im = ax[i // 3, i % 3].imshow(data, origin='lower', cmap=cmap, vmin=zmin, vmax=zmax)
         ax[i // 3, i % 3].set_xlabel('X')
         ax[i // 3, i % 3].set_ylabel('Y')
         ax[i // 3, i % 3].set_title(format_title(titles[i]))
+
+    for i in range(num, nrows * ncols):
+        row, col = divmod(i, ncols)
+        fig.delaxes(ax[row, col])
 
     cbar = fig.colorbar(im, ax=ax, orientation='vertical', fraction=0.03, pad=0.05, location='right')
     cbar.set_label('Time')
     return fig, ax
 
 
-def plot2d_f(data, fields: List[accept_fields], index: int = 0):
-    return plot2d([getattr(data, field)[index] for field in fields], fields)
+def plot2d_f(data, fields: List[accept_fields], index: int = 0, cmap='viridis'):
+    return plot2d([getattr(data, field)[index] for field in fields], fields, cmap)
 
-def plot3d(data_list, titles: List[str]):
+def plot3d(data_list, titles: List[str], cmap='viridis'):
     num = len(data_list)
-    nrows = math.ceil(num / 3)
-    figsize = (15, 6 * nrows)
-    fig, ax = plt.subplots(nrows, 3, figsize=figsize, subplot_kw={'projection': '3d'}, squeeze=False)
+    ncols = 3
+    if num < ncols:
+        ncols = num
+    nrows = math.ceil(num / ncols)
+    figsize = (5 * ncols, 6 * nrows)
+    fig, ax = plt.subplots(nrows, ncols, figsize=figsize, subplot_kw={'projection': '3d'}, squeeze=False)
     im = None
     zmin, zmax = None, None
 
@@ -64,7 +74,7 @@ def plot3d(data_list, titles: List[str]):
         data = data.squeeze()
         y, x = data.nonzero()
         time_values = data[y, x]
-        im = ax[i // 3, i % 3].scatter(x, y, time_values, c=time_values, cmap='blues', vmin=zmin, vmax=zmax)
+        im = ax[i // 3, i % 3].scatter(x, y, time_values, c=time_values, cmap=cmap, vmin=zmin, vmax=zmax)
         ax[i // 3, i % 3].set_xlabel('X')
         ax[i // 3, i % 3].set_ylabel('Y')
         ax[i // 3, i % 3].set_zlabel('Time', rotation=90)
@@ -72,9 +82,13 @@ def plot3d(data_list, titles: List[str]):
         ax[i // 3, i % 3].view_init(elev=30, azim=-45, roll=0)
         ax[i // 3, i % 3].set_box_aspect((1, 1, 1), zoom=0.8)
 
+    for i in range(num, nrows * ncols):
+        row, col = divmod(i, ncols)
+        fig.delaxes(ax[row, col])
+
     cbar = fig.colorbar(im, ax=ax, orientation='vertical', fraction=0.03, pad=0.05, location='right')
     cbar.set_label('Time')
     return fig, ax
 
-def plot3d_f(data, fields: List[accept_fields], index: int = 0):
-    return plot3d([getattr(data, field)[index] for field in fields], fields)
+def plot3d_f(data, fields: List[accept_fields], index: int = 0, cmap='viridis'):
+    return plot3d([getattr(data, field)[index] for field in fields], fields, cmap)
